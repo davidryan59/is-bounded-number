0154a8137634fea67d3411cae8cc8ac763f185fa
David Ryan <davidryan1998@hotmail.com>
2019-01-08 13:40:59 +0700

    Now ES5 code is linted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-var isBoundedNumber = function(input, limit) {
-
+var isBoundedNumber = function isBoundedNumber( input, limit ) {
   // Check an input is numeric and bounded
   // Default bound is a bit lower than Number.MAX_SAFE_INTEGER, which is approx 9e15
 
   // This fails for non-numeric, and also Infinity and NaN
-  if (!Number.isFinite(input)) return false
+  if (!Number.isFinite(input)) return false;
 
   // This fails for numbers outside of the bounds specified
-  if (Math.abs(input)>(limit||1e15)) return false
+  if (Math.abs(input) > (limit || 1e15)) return false;
 
   // Otherwise input is a bounded number, return true
-  return true
+  return true;
+};
 
-}
-
-module.exports = isBoundedNumber
+module.exports = isBoundedNumber;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,47 +1,48 @@
-var assert = require('assert')
-var ibn = require('../src/index')
+var assert = require('assert');
+var ibn = require('../src/index');
 
-describe("isBoundedNumber", function() {
-
-  var runTest = function(input, expectedResult, optionalBound, optionalComment) {
-    var actualResult = (optionalBound) ? ibn(input, optionalBound) : ibn(input)
-    var boundText = (optionalBound) ? ", " + optionalBound : ""
-    var commentText = (optionalComment) ? ", " + optionalComment : ""
-    var label = "isBoundedNumber(" + JSON.stringify(input) + boundText + ") = " + expectedResult + commentText
-    it(label, function() {
-      assert.strictEqual(actualResult, expectedResult)
-    })
-  }
+describe('isBoundedNumber', function indexSpec() {
+  var runTest = function runTest( input, expectedResult, optionalBound, optionalComment ) {
+    var actualResult = (optionalBound) ? ibn(input, optionalBound) : ibn(input);
+    var boundText = (optionalBound) ? ', ' + optionalBound : '';
+    var commentText = (optionalComment) ? ', ' + optionalComment : '';
+    var label = 'isBoundedNumber(' + JSON.stringify(input) + boundText + ') = ' + expectedResult + commentText;
+    it(label, function loopTest() {
+      assert.strictEqual(actualResult, expectedResult);
+    });
+  };
 
   var testArray = [
     [0, true],
-    [-0, true, null, "This is -0"],
+    [-0, true, null, 'This is -0'],
     [1, true],
     [42.001011, true],
     [-172738, true],
-    [-1/7070707077, true],
+    [-1 / 7070707077, true],
     [1e15, true],
     [-1e15, true],
-    [1e15+1, false],
-    [-1e15-1, false],
+    [1000, true, 1e6],
+    [1e6, true, 1e6],
+    [-1e6, true, 1e6],
+    [1e15 + 1, false],
+    [-1e15 - 1, false],
     [1e20, false],
     ['aString', false],
     [Infinity, false],
-    [0-Infinity, false],
+    [0 - Infinity, false],
     [NaN, false],
-    [0-NaN, false],
+    [0 - NaN, false],
     [null, false],
     [true, false],
-    [undefined, false],
     [[1, 2, 3], false],
-    ["1", false],
-    [{1:2}, false],
-    [1000, true, 1e6],
-    [1e6, false, 1000]
-  ]
+    ['1', false],
+    [{1: 2}, false],
+    [1e6, false, 1000],
+    [1e6 + 1, false, 1e6],
+    [-1e6 - 1, false, 1e6]
+  ];
 
-  for (var i=0; i<testArray.length; i++) {
-    runTest(testArray[i][0], testArray[i][1], testArray[i][2], testArray[i][3])
+  for (var i = 0; i < testArray.length; i++) {
+    runTest(testArray[i][0], testArray[i][1], testArray[i][2], testArray[i][3]);
   }
-
-})
+});
